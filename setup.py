1b7bf20d6647639a29351a99beb42fc43642e721
WhiteGobo <asd@notavailable.xxx>
2020-11-09 16:31:46 +0100

    updated setup to version 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
             name = 'graphtoarithmetic',
-            version = '0.1',
+            version = '0.2',
             description='helper package for transforming a graph to arithmetic',
             author = 'Richard Fechner',
             packages=['graphtoarithmetic'],
