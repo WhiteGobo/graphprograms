30409585ee14d453a694ec73500d5f73565fa80f
WhiteGobo <asd@notavailable.xxx>
2020-11-17 11:55:56 +0100

    added encoding for shaum

--- networkxarithmetic/networkxarithmetic.py
+++ networkxarithmetic/networkxarithmetic.py
@@ -337,4 +337,5 @@ def tovaluename( node, nodetype_datakey ):
     """
     :todo: replace str(nodetype_datakey) to nodetype_datakey
     """
-    return "value" + str( hashlib.sha1(str(node) + nodetype_datakey) )
+    return "value" + str( hashlib.sha1(\
+            (str(node) + nodetype_datakey).encode('utf-8') ))
